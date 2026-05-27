d1bc3cb0bdbe8fb8191114199e50b2c0795717d4
Quang Long <dquanglong682005@gmail.com>
2026-05-27 19:30:37 +0700

    Update API_BASE_URL and WS_ENDPOINT values

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://came-novels-innovative-prediction.trycloudflare.com";
-export const WS_ENDPOINT = "https://came-novels-innovative-prediction.trycloudflare.com/ws-karaoke";
+export const API_BASE_URL = "https://accounting-originally-abstract-efficiency.trycloudflare.com";
+export const WS_ENDPOINT = "https://accounting-originally-abstract-efficiency.trycloudflare.com/ws-karaoke";
