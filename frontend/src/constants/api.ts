91992d1c345bfec56fa4d7c0b947df1bafa61b77
Quang Long <dquanglong682005@gmail.com>
2026-05-28 15:43:39 +0700

    Update API base URL and WebSocket endpoint

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "https://accounting-originally-abstract-efficiency.trycloudflare.com";
-export const WS_ENDPOINT = "https://accounting-originally-abstract-efficiency.trycloudflare.com/ws-karaoke";
+export const API_BASE_URL = "https://investment-hung-older-creature.trycloudflare.com";
+export const WS_ENDPOINT = "https://investment-hung-older-creature.trycloudflare.com/ws-karaoke";
