c66ed537b0c1db462811ad9fa63692d572f81bc8
Quang Long <dquanglong682005@gmail.com>
2026-05-29 21:50:47 +0700

    Update API_BASE_URL and WS_ENDPOINT to production

--- frontend/src/constants/api.ts
+++ frontend/src/constants/api.ts
@@ -1,2 +1,5 @@
-export const API_BASE_URL = "http://localhost:8080";
-export const WS_ENDPOINT = "http://localhost:8080/ws-karaoke";
+
+export const API_BASE_URL = "https://liabilities-statewide-decorative-ballot.trycloudflare.com";
+
+
+export const WS_ENDPOINT = "wss://liabilities-statewide-decorative-ballot.trycloudflare.com/ws-karaoke";
